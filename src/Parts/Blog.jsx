4ef822136d629fa1d9d5fefa674082c48da986e5
Mochamad Agung Sudrajat <agung.mosu@gmail.com>
2022-04-09 16:13:30 +0700

    Clean Push

--- src/Parts/Blog.jsx
+++
@@ -1,9 +0,0 @@
-function Blog() {
-    return(
-        <div>
-            This made by blog component
-        </div>
-    )
-}
-
-export default Blog
--- src/Parts/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar() {
-    return(
-        <div>
-            This made by Navbar component
-        </div>
-    )
-}
-
-export default Navbar
