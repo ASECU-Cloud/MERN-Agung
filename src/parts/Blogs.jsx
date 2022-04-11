40aa7a663bd336537cf1c12c76d215623bc586cf
Mochamad Agung Sudrajat <agung.mosu@gmail.com>
2022-04-11 11:44:04 +0700

    another CI CD Check

--- src/parts/Blogs.jsx
+++ src/parts/Blogs.jsx
@@ -4,7 +4,7 @@ function Blogs() {
             <h2 className="text-4xl font-bold underline decoration-teal-400 decoration-2 text-slate-800">
                Latest Post
             </h2>
-
+            <p>In Progress</p>
         </div>
      );
 }
