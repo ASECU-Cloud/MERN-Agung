b6415d1568103bc8651ebaa4d1f8420f6e133635
Mochamad Agung Sudrajat <agung.mosu@gmail.com>
2022-04-25 12:17:51 +0700

    some final touch

--- src/parts/SkillConst.jsx
+++ src/parts/SkillConst.jsx
@@ -2,7 +2,6 @@ function SkillConst({links}) {
     
     return ( 
         <div className="flex justify-around">
-
             {links.map((link) =>(
                 <div className="card m-3 px-14 py-3 drop-shadow-lg" key={link.id}>
                     <img  src={link.link} alt="tes" className="w-6" />
